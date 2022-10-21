f18fa822ef10a9374e2c0b89dc8d83a8ec883718
alwar <alwaromoreno@gmail.com>
2022-10-21 18:20:40 +0200

    backend url on storefront

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,3 +1,6 @@
-const URL = 'http://localhost:5000/'
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://e-nothingness.herokuapp.com/'
+    : 'http://localhost:5000/'
 
 export { URL }
