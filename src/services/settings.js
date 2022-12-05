e1b2a9f7dde6d252aca9aae7195cdb169f03c45f
alwar <alwaromoreno@gmail.com>
2022-12-05 13:55:31 +0100

    Change of server hosting

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,6 +1,6 @@
 const URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://e-nothingness.herokuapp.com/'
+    ? 'https://e-nothigness-backend.onrender.com/'
     : 'http://localhost:5000/'
 
 export { URL }
